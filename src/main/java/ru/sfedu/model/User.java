8f4c7c766960d0a13265f7089ba4d9c5ddd34c43
mixmes <MishasMernikov@yandex.ru>
2023-03-04 21:52:20 +0300

    add Notification class

---
+++ src/main/java/ru/sfedu/model/User.java
@@ -0,0 +1,40 @@
+package ru.sfedu.model;
+
+public class User {
+    private String name ;
+    private long id;
+    private String AccessLevel;
+
+    public User() {
+    }
+
+    public User(String name, long id, String accessLevel) {
+        this.name = name;
+        this.id = id;
+        AccessLevel = accessLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccessLevel() {
+        return AccessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+        AccessLevel = accessLevel;
+    }
+}
