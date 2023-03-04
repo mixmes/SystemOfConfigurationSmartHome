84422cd42fa67f0d6e0fe58e4a24147c9125ac18
ovchnnkva <ov4innnickova@yandex.ru>
2023-03-04 22:15:33 +0300

    Add notifications class and Sensor class

---
+++ src/main/java/ru/sfedu/model/Notification.java
@@ -0,0 +1,4 @@
+package ru.sfedu.model;
+
+public class Notification {
+}
---
+++ src/main/java/ru/sfedu/model/Sensor.java
@@ -0,0 +1,10 @@
+package ru.sfedu.model;
+
+public class Sensor {
+    private long id;
+    private String name;
+    public Sensor(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
