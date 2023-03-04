64dd03579df888856c1d376aa59fe3b52c37c0f5
mixmes <MishasMernikov@yandex.ru>
2023-03-04 22:00:31 +0300

    add Sensor class

---
+++ src/main/java/ru/sfedu/model/Sensor.java
@@ -0,0 +1,30 @@
+package ru.sfedu.model;
+
+public class Sensor {
+    private long id;
+    private String name;
+
+    public Sensor() {
+    }
+
+    public Sensor(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
