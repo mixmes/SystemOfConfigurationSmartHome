70240aa9e72bb914dc1dc7a95b72d8987a71f18f
mixmes <MishasMernikov@yandex.ru>
2023-03-05 15:35:23 +0300

    added ConfigurationUtil , config.properties and Constants

---
+++ src/main/java/ru/sfedu/Constants.java
@@ -0,0 +1,16 @@
+package ru.sfedu;
+
+public class Constants {
+        public static final String deviceXML = "deviceXML";
+        public static final String heaterXML = "heaterXML";
+        public static final String humidifierXML = "humidifierXML";
+        public static final String hygrometerXML = "hygrometerXML";
+        public static final String lampXML = "lampXML";
+        public static final String lock = "lockXML";
+        public static final String notificationXML = "notificationXML";
+        public static final String sensorXML = "sensorXML";
+        public static final String smartHomeXML = "smartHomeXML";
+        public static final String socketXML = "socketXML";
+        public static final String termometr = "termometrXML";
+        public static final String userXML = "userXML";
+ }
