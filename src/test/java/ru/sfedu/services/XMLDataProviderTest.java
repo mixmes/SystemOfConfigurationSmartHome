9da2f8b709fa40324bbc8af99c976cd48342ab86
mixmes <MishasMernikov@yandex.ru>
2023-03-05 17:04:29 +0300

    added XMLDataProviderTest

---
+++ src/test/java/ru/sfedu/services/XMLDataProviderTest.java
@@ -0,0 +1,164 @@
+package ru.sfedu.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XMLDataProviderTest {
+
+    @Test
+    void deleteRecord() {
+    }
+
+    @Test
+    void testDeleteRecord() {
+    }
+
+    @Test
+    void testDeleteRecord1() {
+    }
+
+    @Test
+    void saveDeviceRecord() {
+    }
+
+    @Test
+    void getDeviceRecordByID() {
+    }
+
+    @Test
+    void updateDeviceRecord() {
+    }
+
+    @Test
+    void saveHeaterRecord() {
+    }
+
+    @Test
+    void getHeaterRecordByID() {
+    }
+
+    @Test
+    void updateHeaterRecord() {
+    }
+
+    @Test
+    void saveHumidifierRecord() {
+    }
+
+    @Test
+    void getHumidifierRecordByID() {
+    }
+
+    @Test
+    void updateHumidifierRecord() {
+    }
+
+    @Test
+    void saveHygrometerRecord() {
+    }
+
+    @Test
+    void getHygrometerRecordByID() {
+    }
+
+    @Test
+    void updateHygrometerRecord() {
+    }
+
+    @Test
+    void saveLampRecord() {
+    }
+
+    @Test
+    void getLampRecordByID() {
+    }
+
+    @Test
+    void deleteLampRecord() {
+    }
+
+    @Test
+    void saveLockRecord() {
+    }
+
+    @Test
+    void getLockRecordByID() {
+    }
+
+    @Test
+    void deleteLockRecord() {
+    }
+
+    @Test
+    void saveNotificationRecord() {
+    }
+
+    @Test
+    void getNotificationRecordByID() {
+    }
+
+    @Test
+    void updateNotificationRecord() {
+    }
+
+    @Test
+    void saveSensorRecord() {
+    }
+
+    @Test
+    void getSensorRecordByID() {
+    }
+
+    @Test
+    void updateSensorRecord() {
+    }
+
+    @Test
+    void saveSmartHomeRecord() {
+    }
+
+    @Test
+    void getSmartHomeRecordByID() {
+    }
+
+    @Test
+    void updateSmartHomeRecord() {
+    }
+
+    @Test
+    void saveSocketRecord() {
+    }
+
+    @Test
+    void getSocketRecordByID() {
+    }
+
+    @Test
+    void updateSocketRecord() {
+    }
+
+    @Test
+    void saveTermometrRecord() {
+    }
+
+    @Test
+    void getTermometrRecordByID() {
+    }
+
+    @Test
+    void updateTermometrRecord() {
+    }
+
+    @Test
+    void saveUserRecord() {
+    }
+
+    @Test
+    void getUserRecordByID() {
+    }
+
+    @Test
+    void updateUserRecord() {
+    }
+}
