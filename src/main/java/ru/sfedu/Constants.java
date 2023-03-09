bef488f8143eee35c31fcdd8f64177dd628d8be9
mixmes <MishasMernikov@yandex.ru>
2023-03-09 22:50:11 +0300

    CSV files and configs

--- src/main/java/ru/sfedu/Constants.java
+++ src/main/java/ru/sfedu/Constants.java
@@ -26,6 +26,17 @@ public class Constants {
         public static final String SOCKET_XML = "socketXML";
         public static final String TERMOMERT_XML = "termometrXML";
         public static final String USER_XML = "userXML";
+        public static final String HEATER_CSV = "heaterCSV";
+        public static final String HUMIDIFIER_CSV = "humidifierCSV";
+        public static final String HYGROMETER_CSV = "hygrometerCSV";
+        public static final String LAMP_CSV = "lampCSV";
+        public static final String LOCK_CSV = "lockCSV";
+        public static final String NOTIFICATION_CSV = "notificationCSV";
+        public static final String SMART_HOME_CSV = "smartHomeCSV";
+        public static final String SOCKET_CSV = "socketCSV";
+        public static final String TERMOMETER_CSV = "termometerCSV";
+        public static final String USER_CSV = "userCSV";
+
 
         public enum AccessLevel {
                 ADMIN,
